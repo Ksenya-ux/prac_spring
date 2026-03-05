211711e5b127ff1838ca6e5d155ad8ef22224317
Kseniya <krikovskaya06@gmail.com>
2026-03-05 14:05:30 +0300

    Функция read_git_object находит и распаковывает объекты Git и проверяет на повреждение и отсутсвтие  файлов

--- 20260219/1/prog.py
+++ 20260219/1/prog.py
@@ -4,3 +4,12 @@ from pathlib import Path
 def get_git_dir(repo_path):
 	git_dir = os.path.join(repo_path, '.git')
 	return git_dir if os.path.isdir(git_dir) else None
+def read_git_obj(repo_path, obj_hash):
+	obj_path = os.path.join(repo_path, '.git', 'objects', obj_hash[:2], obj_hash[2:])
+    try:
+        with open(obj_path, 'rb') as f:
+            compressed_data = f.read()
+        return zlib.decompress(compressed_data)
+    except (FileNotFoundError, zlib.error):
+        print(f"Git object {object_hash} not found or corrupted", file=sys.stderr)
+        sys.exit(1)
