11bd2336001d4d481b48b16b3fab81b77871ffbc
Kseniya <krikovskaya06@gmail.com>
2026-03-08 00:32:22 +0300

    добавление перемещения

--- 20260305/1/prog.py
+++ 20260305/1/prog.py
@@ -5,3 +5,20 @@ class MUD:
     def __init__(self):
         self.field = [[None for _ in range(10)] for _ in range(10)]
         self.player_position = (0, 0)
+	def move_player(self, direction):
+        x, y = self.player_position
+        if direction == 'up':
+            y = (y - 1) % 10
+        elif direction == 'down':
+            y = (y + 1) % 10
+        elif direction == 'left':
+            x = (x - 1) % 10
+        elif direction == 'right':
+            x = (x + 1) % 10
+        else:
+            print("Invalid command")
+            return
+
+        self.player_position = (x, y)
+        print(f"Moved to ({x}, {y})")
+        self.encounter(x, y)
